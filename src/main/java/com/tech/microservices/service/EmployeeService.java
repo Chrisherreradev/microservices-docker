7467bfd7651193df28e48bd5f2cbac4f646222ab
chris <christianherrera@live.com.mx>
2023-03-18 17:59:07 -0600

    Post method to create a new employee implemented

--- src/main/java/com/tech/microservices/service/EmployeeService.java
+++ src/main/java/com/tech/microservices/service/EmployeeService.java
@@ -1,4 +1,8 @@
 package com.tech.microservices.service;
 
+import com.tech.microservices.model.Employee;
+
 public interface EmployeeService {
+
+    Employee save(Employee employee);
 }
--- src/main/java/com/tech/microservices/service/EmployeeServiceImpl.java
+++ src/main/java/com/tech/microservices/service/EmployeeServiceImpl.java
@@ -1,4 +1,21 @@
 package com.tech.microservices.service;
 
+import com.tech.microservices.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
+    List<Employee> employees = new ArrayList<>();
+    @Override
+    public Employee save(Employee employee) {
+        if(employee.getEmployeeId()==null || employee.getEmail()==null){
+            employee.setEmployeeId(new UUID(0L, 0L).getMostSignificantBits() & Long.MAX_VALUE);
+        }
+        employees.add(employee);
+        return employee;
+    }
 }
